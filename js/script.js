ad52e39bbce8a34546148d696ebe60b48a6d966b
Junaid Qureshi <Junaid.562000@gmail.com>
2025-01-02 13:49:04 +0530

    add typing effect to the main heading

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 // Add custom JavaScript here
+
+function runTypingEffect() {
+  let text = "I am John Doe.";
+  const typingElement = document.querySelector("#typing-text");
+  const typingDelay = 100;
+
+  typeText(text, typingElement, typingDelay);
+}
+
+function typeText(text, element, delay) {
+  for (let i = 0; i < text.length; i++) {
+    setTimeout(() => {
+      element.textContent += text[i];
+    }, delay * i);
+  }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  runTypingEffect();
+});
